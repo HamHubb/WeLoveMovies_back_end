6b3ebc2813070cfbd92bf0e37e039e1cd58d63e4
Hairo Morales <moraleshairo@gmail.com>
2023-12-24 21:41:41 -0800

    updated app.js again

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ const app = express();
 app.use(cors())
 app.use((req,res,next)=> {
     res.header('Access-Control-Allow-Origin', 'https://starter-movie-front-end-wj5u.onrender.com')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 })
 app.use(express.json())
 
